06a3444950afc58e05264cfce23e3e2370112e79
Xayah <ycxiang14sep@gmail.com>
2025-09-20 21:41:22 +0900

    feat: improve CPU AVX2 detection for cross-platform compatibility

--- src/backend/registry/cpu.cpp
+++ src/backend/registry/cpu.cpp
@@ -1,33 +1,40 @@
 #include "cpu.h"
 #if defined(_MSC_VER)
-  #include <intrin.h>
+  #if defined(_M_X64) || defined(_M_IX86)
+    #include <intrin.h>
+  #endif
 #else
-  #include <cpuid.h>
+  #if defined(__x86_64__) || defined(__i386__)
+    #include <cpuid.h>
+  #endif
 #endif
 
 namespace sim {
     bool cpu_has_avx2() {
-        // Check CPUID leaf 7, EBX bit 5 (AVX2)
+    #if defined(_MSC_VER) && (defined(_M_X64) || defined(_M_IX86))
+        // MSVC on x86/x64: use __cpuid/__cpuidex
         int regs[4] = {0,0,0,0};
-    #if defined(_MSC_VER)
         __cpuid(regs, 0);
         int max_leaf = regs[0];
         if (max_leaf >= 7) {
-            int regs7[4];
+            int regs7[4] = {0,0,0,0};
             __cpuidex(regs7, 7, 0);
-            return (regs7[1] & (1 << 5)) != 0; // EBX bit 5
+            return (regs7[1] & (1 << 5)) != 0; // EBX bit 5 indicates AVX2
         }
         return false;
-    #else
+    #elif (defined(__x86_64__) || defined(__i386__)) && (defined(__GNUC__) || defined(__clang__))
+        // GCC/Clang on x86/x64: use <cpuid.h>
         unsigned int eax=0, ebx=0, ecx=0, edx=0;
         __cpuid(0, eax, ebx, ecx, edx);
         if (eax >= 7) {
-            unsigned int eax7, ebx7, ecx7, edx7;
+            unsigned int eax7=0, ebx7=0, ecx7=0, edx7=0;
             __cpuid_count(7, 0, eax7, ebx7, ecx7, edx7);
-            return (ebx7 & (1u << 5)) != 0;
+            return (ebx7 & (1u << 5)) != 0; // EBX bit 5 indicates AVX2
         }
+        return false;
+    #else
+        // Non-x86 architectures (e.g., ARM/Apple Silicon): AVX2 not available
         return false;
     #endif
     }
 }
-
