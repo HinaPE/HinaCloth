90ac9de8865ab50e7c745a484bc2b1fad48f3dd0
Xayah <ycxiang14sep@gmail.com>
2025-09-23 18:59:00 +0900

    refactor: remove unused namespace declarations in cooking.h

--- src/cooking/cooking.h
+++ src/cooking/cooking.h
@@ -10,7 +10,6 @@
 #include "core/common/types.h"
 
 namespace sim {
-    namespace eng { struct BuildDesc; struct Command; }
     [[nodiscard]] bool cooking_build_model(const eng::BuildDesc& in, Model*& out);
     [[nodiscard]] bool cooking_rebuild_model_from_commands(const Model& cur, const eng::Command* cmds, std::size_t count, Model*& out, RemapPlan*& plan);
 }
